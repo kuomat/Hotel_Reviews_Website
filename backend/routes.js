af98b20cb67b8ab621cd4c1e4ec9883d9092e463
ellazarinis <ellaz@seas.upenn.edu>
2024-04-21 14:18:33 -0400

    yesterday's changes

--- backend/routes.js
+++ backend/routes.js
@@ -19,55 +19,54 @@ const recentReviews = async function (req, res) {
     return res.status(400).json({error: 'Hotel name not provided.'});
   }
 
-  try {
-    const data = await connection.query(`
-      SELECT a.hotel_name, f.date, f.review, f.overall_score
-      FROM address_df a JOIN final_df f ON a.address = f.address
-      INNER JOIN (
-        SELECT address, MAX(date) AS max_date
-        FROM final_df
-        WHERE a.hotel_name = '${hotelName}'
-        GROUP BY address
-      ) AS latest ON f.address = latest.address AND f.date = latest.max_date
+  connection.query(`
+    SELECT a.hotel_name, f.date, f.review, f.overall_score
+    FROM address_df a JOIN final_df f ON a.address = f.address
+    INNER JOIN (
+      SELECT address, MAX(date) AS max_date
+      FROM final_df
       WHERE a.hotel_name = '${hotelName}'
-      ORDER BY a.hotel_name, f.date DESC;
-    `);
+      GROUP BY address
+    ) AS latest ON f.address = latest.address AND f.date = latest.max_date
+    WHERE a.hotel_name = '${hotelName}'
+    ORDER BY a.hotel_name, f.date DESC;
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      res.status(404).json({error: err});
+    } else {
+      res.status(200).json(data.json);
+    }
+  });
 
-    console.log(data.json);
-    res.status(200).json({message: 'success'});
-  } catch (e) {
-    res.status(404).json({'error': e});
-  }
 }
 
 // Route 2
 const hotelsWithBestCategScore = async function (req, res) {
-  try {
-    const data = await connection.query(`
-      (SELECT 'Service' AS category, a.hotel_name, AVG(f.service_score) AS avg_score
-      FROM address_df a JOIN final_df f ON a.address = f.address
-      GROUP BY a.hotel_name
-      ORDER BY avg_score DESC
-      LIMIT 1)
-      UNION ALL
-      (SELECT 'Cleanliness' AS category, a.hotel_name, AVG(f.cleanliness_score) AS avg_score
-      FROM address_df a JOIN final_df f ON a.address = f.address
-      GROUP BY a.hotel_name
-      ORDER BY avg_score DESC
-      LIMIT 1)
-      UNION ALL
-      (SELECT 'Value' AS category, a.hotel_name, AVG(f.value_score) AS avg_score
-      FROM address_df a JOIN final_df f ON a.address = f.address
-      GROUP BY a.hotel_name
-      ORDER BY avg_score DESC
-      LIMIT 1);
-  `);
-
-    console.log(data.json);
-    res.status(200).json({message: 'success'});
-  } catch (e) {
-    res.status(404).json({'error': e});
-  }
+  connection.query(`
+    (SELECT 'Service' AS category, a.hotel_name, AVG(f.service_score) AS avg_score
+    FROM address_df a JOIN final_df f ON a.address = f.address
+    GROUP BY a.hotel_name
+    ORDER BY avg_score DESC
+    LIMIT 1)
+    UNION ALL
+    (SELECT 'Cleanliness' AS category, a.hotel_name, AVG(f.cleanliness_score) AS avg_score
+    FROM address_df a JOIN final_df f ON a.address = f.address
+    GROUP BY a.hotel_name
+    ORDER BY avg_score DESC
+    LIMIT 1)
+    UNION ALL
+    (SELECT 'Value' AS category, a.hotel_name, AVG(f.value_score) AS avg_score
+    FROM address_df a JOIN final_df f ON a.address = f.address
+    GROUP BY a.hotel_name
+    ORDER BY avg_score DESC
+    LIMIT 1);
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      res.status(404).json({error: err});
+    } else {
+      res.status(200).json(data.json);
+    }
+  });
 }
 
 // Route 3
@@ -114,19 +113,22 @@ const hotelsScore = async function (req, res) {
 
 // Route 7
 const reviewsPerHotel = async function (req, res) {
-  try {
-    const data = await connection.query(`
+  
+
+
+  connection.query(`
     SELECT a.hotel_name, COUNT(f.primary_key) AS review_count
     FROM address_df a
     JOIN final_df f ON a.address = f.address
     GROUP BY a.hotel_name
-    ORDER BY review_count DESC;`);
-
-    console.log(data.json);
-    res.status(200).json({'message': 'success'});
-  } catch (e) {
-    res.status(404).json({'error': e});
-  }
+    ORDER BY review_count DESC;
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      res.status(404).json({error: err});
+    } else {
+      res.status(200).json({data.json});
+    }
+  });
 }
 
 // Route 8
